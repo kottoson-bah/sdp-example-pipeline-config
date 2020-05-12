69b03f685ddd927bc50be0596c44834e4ebabbf8
kottoson-bah <45176300+kottoson-bah@users.noreply.github.com>
2020-05-12 15:19:38 -0400

    Create pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,30 @@
+application_environments{
+  dev{
+    short_name = "dev"
+    long_name = "Development"
+  }
+}
+
+libraries{
+  sdp{
+    images{
+      registry = "https://docker.pkg.github.com"
+      repository = "boozallen/sdp-images"
+      cred = "github"
+    }
+  }
+  github
+  docker{
+    merge = true
+    registry = "docker-registry-default.apps.okd.kottoson.net"
+    repo_path_prefix = "sample-app"
+    cred = "openshift-sample-app-tiller"
+  }
+  openshift{
+    url = "master.okd.kottoson.net:8443"
+    helm_configuration_repository = "https://github.com/kottoson-bah/sdp-example-helm-config.git"
+    helm_configuration_repository_credential = "github"
+    tiller_namespace = "sample-app-tiller"
+    tiller_credential = "openshift-sample-app-tiller"
+  }
+}
